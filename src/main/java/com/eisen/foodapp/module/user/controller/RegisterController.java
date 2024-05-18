8bf6d1ff740ab6a0621712c978493c39c436c785
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2024-05-17 22:10:49 -0300

    feat: set default role(CLIENT) for user on register

--- src/main/java/com/eisen/foodapp/module/user/controller/RegisterController.java
+++ src/main/java/com/eisen/foodapp/module/user/controller/RegisterController.java
@@ -2,6 +2,7 @@ package com.eisen.foodapp.module.user.controller;
 
 import com.eisen.foodapp.module.user.dto.RegisterDTO;
 import com.eisen.foodapp.module.user.model.User;
+import com.eisen.foodapp.module.user.repository.RoleRepository;
 import com.eisen.foodapp.module.user.repository.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,14 +22,21 @@ public class RegisterController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     @PostMapping
     public ResponseEntity register(@Valid @RequestBody RegisterDTO data) {
         if (this.userRepository.findByLogin(data.login()) != null) return ResponseEntity.badRequest().build();
+
+        var role = roleRepository.findByRole("CLIENT");
 
         var user = new User();
         user.setName(data.name());
         user.setLogin(data.login());
         user.setPassword(data.password());
+
+        user.addRole(role);
 
         userRepository.save(user);
 
