433a4ca0e8de686b0a96c7815bf29524c0483e94
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2024-05-17 21:42:38 -0300

    feat: created role model

--- src/main/java/com/eisen/foodapp/module/user/model/Role.java
+++ src/main/java/com/eisen/foodapp/module/user/model/Role.java
@@ -1,17 +1,40 @@
 package com.eisen.foodapp.module.user.model;
 
-public enum Role {
-    ADMIN("admin"),
-    CLIENT("client"),
-    ;
+import jakarta.persistence.*;
 
-    private final String role;
+@Entity
+public class Role {
+    @Id()
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-    Role(String role) {
-        this.role = role;
+    @Column
+    private String name;
+
+    @Column
+    private String role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getRole() {
         return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
