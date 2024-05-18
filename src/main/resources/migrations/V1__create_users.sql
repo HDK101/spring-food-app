092103bdea79efc9ede7d9475df549cd0808050d
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2024-05-18 17:44:58 -0300

    fix: change seqs increments in migrations

--- src/main/resources/migrations/V1__create_users.sql
+++ src/main/resources/migrations/V1__create_users.sql
@@ -1,2 +1,2 @@
-create sequence users_seq start with 1 increment by 50;
+create sequence users_seq start with 1 increment by 1;
 create table users (id bigint not null, login varchar(255), name varchar(255), password_hash varchar(255), primary key (id));
--- src/main/resources/migrations/V2__create_roles.sql
+++ src/main/resources/migrations/V2__create_roles.sql
@@ -1,2 +1,2 @@
-create sequence roles_seq start with 1 increment by 50;
+create sequence roles_seq start with 1 increment by 1;
 create table roles (id bigint not null, name varchar(255), role varchar(255), primary key (id));
